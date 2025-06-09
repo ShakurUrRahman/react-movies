997b2bbca050f815639ca31cc5c123741cc82bd1
ShakurUrRahman <shakururrahman@gmail.com>
2025-06-09 15:38:43 +0600

    Corrected env variables

--- src/appwrite.js
+++ src/appwrite.js
@@ -3,7 +3,12 @@ import { Client, Databases, ID, Query } from "appwrite";
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
-const ENDPOINT_URL = import.meta.env.VITE_APPWRITE_ENDPOINT;
+
+const endpoint = import.meta.env.VITE_APPWRITE_ENDPOINT;
+
+if (!endpoint) {
+	console.error("❌ Missing VITE_APPWRITE_ENDPOINT in environment variables");
+}
 
 const client = new Client()
 	.setEndpoint("https://fra.cloud.appwrite.io/v1")
